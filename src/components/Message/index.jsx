3ca37030530c04a35264b453bd115494bb6d627e
Gabriel Oczust <gabriel.oczust@gmail.com>
2021-02-09 14:39:12 -0300

    # Message Component - Close button created

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { hideMessage } from '../../store/layout';
 
 import './styles.css';
 
 export default function Message() {
 
+    const dispatch = useDispatch();
     const message = useSelector(state => state.layout.message);
 
     const title = {
         success: "Sucesso",
         primary: "Aviso",
         danger: "Erro",
-        warning: "Alerta",
+        warning: "Alerta"
     };
+
+    const handleClose = () => {
+        dispatch(hideMessage());
+    }
 
     return (
         <>
@@ -21,6 +27,9 @@ export default function Message() {
                     <div className="toast-header">
                         <span className={`badge badge-${message.color} mr-2`}> </span>
                         <strong className="mr-auto">{title[message.color]}</strong>
+                        <button type="button" className="close" data-dismiss="toast" aria-label="Close" onClick={handleClose}>
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
                     <div className="toast-body">
                         {message.text}
